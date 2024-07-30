a51213fdf0564f1bdd02a49c7e80313574248cc1
nlibenn <147007074+nlibenn@users.noreply.github.com>
2024-07-30 14:46:16 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+import { Timer } from './src/features/Timer';
+
+export default function App() {
+const [currentSubject, setCurrentSubject] = useState('test');
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <Timer
+          focusSubject={currentSubject}
+          onTimerEnd={() => {}}
+          clearSubject={() => {
+            setCurrentSubject(null);
+          }}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.lightorange,
+  },
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,5 @@
+export const colors = {
+  white: '#ffffff',
+  lightorange: '#EFC7C3',
+  progressbar: '#918365'
+}
